d2a5445e0ec7fdee18d94b5f1d40007334f58104
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2017-02-09 12:39:43 -0500

    :penguin: Small Update

---
+++ Week4/Lab/04_Feb9Lecture.js
@@ -0,0 +1,48 @@
+/* Some of the Feb 9th lecture examples in random order */
+
+// callback function example
+function printBanana(x, yell) {
+  if (typeof yell !== "function"){
+    throw new Err....
+  }
+  if (1 + x > 0){
+    console.log("banana!");
+  } else {
+    yell();
+  }
+}
+
+function myAwesomeYelling() {
+  throw new Error("baaaa");
+}
+
+printBanana(-5, myAwesomeYelling)
+
+o = {"day": "Wed",  isToday: true};
+
+// x is a function
+var x = getProp("isToday", o);
+// y is true
+var y = x();
+// w is true
+var w = getProp("isToday", o)();
+
+o.isToday
+o["isToday"]
+
+function getProp(name, obj) {
+  // creating a closure
+  var result = function() {
+    if(name in obj) { // obj[name] === "undefined"
+      // obj.name.toString() same as obj["name"].toString()
+      // obj[name.toString()]
+      // obj["isToday"]
+      return obj[name]; // Why not obj.name? it's obj["name"]
+    } else {
+      // throw new Error("message");
+      console.log("Doesn't exist");
+      return false;
+    }
+  };
+  return result;
+}
---
+++ Week4/Lab/lab.js
@@ -0,0 +1,6 @@
+const myList = [
+{ title: 'title1', value: 'value1', visible: true },
+{ title: 'title2', value: 'value2', visible: false },
+{ title: 'title3', value: 'value3', visible: true },
+{ title: 'title4', value: 'value4' }
+];

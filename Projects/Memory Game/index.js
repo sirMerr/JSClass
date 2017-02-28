7bdbf3dbbfe14181e1be2b0f4b5bf3b8e98483bf
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2017-02-27 22:29:11 -0500

    :wrench: Fix click issues and finish up game

--- Projects/Memory Game/index.js
+++ Projects/Memory Game/index.js
@@ -1,37 +1,69 @@
-/* global document: true */
+/**
+ * Assignment #1 Part 2
+ * @author Tiffany Le-Nguyen
+ * For 420-423-DW Internet Applications II – Winter 2017
+ */
 
+/* global document: true window: true Image: true U: true */
+
+// global namespace
 const g = {};
 
 document.addEventListener('DOMContentLoaded', () => {
 	g.tiles = document.querySelectorAll('.tiles');
+	g.startButton = document.querySelector('.start');
 	g.stopButton = document.querySelector('.stop');
 	g.cheatButton = document.querySelector('.cheat');
 	g.coverLetters = document.querySelectorAll('.tiles h2');
 	g.coverImages = document.querySelectorAll('.tiles img');
 	g.layer2Images = document.querySelectorAll('.layer2 img');
+	g.layer2 = document.querySelector('.layer2');
 	g.images = ['images/svg/abacus.svg', 'images/svg/baby.svg', 'images/svg/toy-1.svg',
 		'images/svg/book.svg', 'images/svg/food.svg', 'images/svg/game.svg',
 		'images/svg/hat.svg', 'images/svg/teddy-bear.svg', 'images/svg/abacus.svg',
 		'images/svg/baby.svg', 'images/svg/toy-1.svg', 'images/svg/book.svg',
 		'images/svg/food.svg', 'images/svg/game.svg', 'images/svg/hat.svg',
 		'images/svg/teddy-bear.svg'];
+	g.backgroundImages = ['images/background.jpg', 'images/background2.jpg', 'images/background3.jpg',
+		'images/background4.jpg', 'images/background5.jpg', 'images/background6.jpg'];
 	g.holderImage = '';
 	g.holderImage.visibility = '';
+	g.holderCover = '';
 	g.clicks = 0;
+	g.holderBg = 0;
+
+	cacheImages();
+	defaultTiles();
 
 	// add event listeners
-	g.stopButton.addEventListener('click', stop);
-	g.cheatButton.addEventListener('click', cheat);
-
-	defaultTiles();
+	U.addEvent(g.startButton, 'click', start);
+	U.addEvent(g.stopButton, 'click', stop);
+	U.addEvent(g.cheatButton, 'click', cheat);
+	U.addEvent(document.body, 'keypress', displayKey);
 });
 
+/**
+ * Restarts the game
+ * @method start
+ */
+function start() {
+	defaultTiles();
+	for (let i = 0; i < g.tiles.length; i++) {
+		g.tiles[i].style.visibility = 'visible';
+		g.layer2Images[i].style.visibility = 'visible';
+		// console.log(g.tiles[i]);
+	}
+}
 /**
  * Reveal background image. Ends the game
  * @method stop
  */
 function stop() {
-	setTilesBg('transparent');
+	for (let i = 0; i < g.tiles.length; i++) {
+		g.tiles[i].style.visibility = 'hidden';
+		g.layer2Images[i].style.visibility = 'hidden';
+		// console.log(g.tiles[i]);
+	}
 }
 
 /**
@@ -52,37 +84,58 @@ function cheat() {
 /**
  * Opens tile depending on click/keypress
  * @method openTile
- * @param  {Event} e  -- event object
+ * @param  {Event} e  event object
  */
 function openTile(e) {
+	g.clicks++;
+	if (g.clicks > 2) {
+		g.clicks--;
+		return;
+	}
+	// console.log(e.srcElement);
 	const letter = e.srcElement.innerHTML;
-	//e.srcElement.parentElement.style.visibility = 'hidden';
-
+	e.srcElement.parentElement.style.visibility = 'hidden';
+	const img = findImage(letter);
 	if (g.holderImage === '') {
-		console.log('hi!');
-		g.holderImage = findImage(letter);
-		g.holderImage.style.visibility = 'hidden';
-		console.log(g.holderImage.style.visibility);
-		// g.holderImage.style.visibility = 'hidden';
-	} else if (g.holderImage.src === findImage(letter).src) {
-		console.log('yay!');
+		// console.log('New holders');
+		g.holderImage = img;
+		g.holderCover = e.srcElement.parentElement;
+	} else if (g.holderImage.src === img.src) {
+		// console.log('It\'s a match!');
+		setVisibility(g.holderImage, img);
 	} else {
-		console.log('boo');
-		g.holderImage = '';
-		// g.holderImage.style.visibility = 'visible';
-		console.log('invisible time!');
-		// e.srcElement.parentElement.style.visibility = 'visible';
+		// console.log('Not a match');
+		setVisibility(g.holderCover, e.srcElement.parentElement);
 	}
 }
 
 /**
+ * Sets visibility of two images to hidden or visible
+ * depending on what they are initially
+ * @method setVisibility
+ * @param  {Image}      img1
+ * @param  {Image}      img2
+ */
+function setVisibility(img1, img2) {
+	window.setTimeout(() => {
+		img1.style.visibility = img1 !== '' && img1.style.visibility === 'hidden' ? 'visible' : 'hidden';
+		img2.style.visibility = img2 !== '' && img2.style.visibility === 'hidden' ? 'visible' : 'hidden';
+		g.clicks -= 2;
+	}, 1000);
+
+	g.holderImage = '';
+	g.holderCover = '';
+}
+/**
  * Finds image corresponding to letter
  * @method findImage
- * @param  {String}  letter -- letter [a-z]
- * @return {Object}         -- corresponding element
+ * @param  {String}  letter  letter [a-z]
+ * @return {Object}          corresponding element
  */
 function findImage(letter) {
 	for (let i = 0; i < g.layer2Images.length; i++) {
+		// console.log(letter);
+		// console.log(g.layer2Images[i].classList);
 		if (g.layer2Images[i].classList.contains(letter)) {
 			return g.layer2Images[i];
 		}
@@ -91,24 +144,11 @@ function findImage(letter) {
 }
 
 /**
- * Finds image corresponding to letter
- * @method findImage
- * @param  {String}  letter -- letter [a-z]
- * @return {Object}         -- corresponding element
- */
-function findCover(letter) {
-	for (let i = 0; i < g.coverLetters.length; i++) {
-		if (g.coverLetters[i].innerHTML === letter) {
-			return g.coverLetters[i];
-		}
-	}
-	return '';
-}
-
-/**
  * Sets default tiles to a background
+ * @method defaultTiles
  */
 function defaultTiles() {
+	g.clicks = 0;
 	// randomize image array
 	g.images = randomizeArray(g.images);
 
@@ -117,39 +157,80 @@ function defaultTiles() {
 		g.coverImages[i].src = 'images/pokemon.gif';
 		g.layer2Images[i].src = g.images[i];
 	}
+
+	g.layer2.style.backgroundImage = 'url(' + getNextBg() + ')';
+
+	g.coverImages.forEach(element => {
+		if (element.style.opacity !== '1') {
+			element.style.opacity = 1;
+		}
+	});
 }
 
-function randomizeArray(array) {
-	let currentIndex = array.length;
-	let temporaryValue;
-	let randomIndex;
-
-  // While there remain elements to shuffle
-	while (currentIndex !== 0) {
-    // Pick a remaining element
-		randomIndex = randomNum(currentIndex, 0);
-		currentIndex--;
-
-    // And swap it with the current element.
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
+/**
+ * Gets next background from g.backgroundImages array
+ * @method getNextBg
+ * @return {String}  image link from ./images
+ */
+function getNextBg() {
+	if (g.holderBg === g.backgroundImages.length - 1) {
+		g.holderBg = 0;
+	} else {
+		g.holderBg++;
 	}
+	// console.log(g.holderBg);
+	return g.backgroundImages[g.holderBg];
+}
 
+/**
+ * Sorting the array using Array.Prototype.sort.
+ *
+ * NOTE: As the requirement is to be cross-compatible,
+ * I decided to use this sort because we are randomizing it with
+ * the custom comparison function, which I felt removed the non-stability
+ * issue of browsers that use Array.Prototype.sort. differently.
+ * @method randomizeArray
+ * @param  {Array}       array	array to shuffle
+ * @return {Array}             	randomized array
+ */
+function randomizeArray(array) {
+	array.sort((a, b) => {
+		// 0.7 - [0, 1)
+		return 0.7 - Math.random();
+	});
+
+	// array.forEach(element => {
+	// 	console.log(element);
+	// })
 	return array;
 }
+
 /**
- * Returns a random number (int) such as: (min, max[
- * @param  {Number} min -- inclusive min
- * @param  {Number} max -- non-inclusive max
- * @return {Number}     -- random number integer
+ * Caches images
+ * @method cacheImages
  */
-function randomNum(min, max) {
-	return Math.floor(Math.random() * (max - min)) + min;
+function cacheImages() {
+	const array = [];
+	const image = new Image();
+
+	g.images.forEach(element => {
+		image.src = element;
+		array.push(image);
+	});
 }
 
-function setTilesBg(value) {
-	for (let i = 0; i < g.tiles.length; i++) {
-		g.tiles[i].style.background = value;
+/**
+ * Triggers a click event on the cover tiles
+ * if there is a corresponding keypress
+ * @method displayKey
+ * @param  {Event}   e
+ */
+function displayKey(e) {
+	const evt = e || window.event;
+	const charCode = evt.which || evt.keyCode;
+	const target = document.querySelector(`.tile${charCode - 96} h1`);
+
+	if (target !== null && charCode < 113 && target.style.visibility !== 'hidden') {
+		target.click();
 	}
 }

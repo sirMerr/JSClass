69185f483bbdb924c3513cecc41bcdc5eddbd43c
sirMerr <letiffany.nguyen@gmail.com>
2017-05-07 01:20:32 -0400

    :package: Document and optimize encryption/decryption

--- Projects/Project2/index.js
+++ Projects/Project2/index.js
@@ -6,35 +6,7 @@
 
 /* global U modernBrowser emojis asciiKeys document lettersToEmojiObj emojisToLettersObj*/
 const g = {};
-// var emojiStringToArray = function (str) {
-//   split = str.split(/([\uD800-\uDBFF][\uDC00-\uDFFF])/);
-//   arr = [];
-//   for (var i=0; i<split.length; i++) {
-//     char = split[i]
-//     if (char !== "") {
-//       arr.push(char);
-//     }
-//   }
-//   return arr;
-// };
 
-// function showOutput(output) {
-//     var emojisArray = emojiStringToArray(output);
-//     // makes sure output will not repeat
-//     g.output.value = '';
-//     var counter = 0;
-//     var interval = setInterval(typeOutput, 100);
-
-//     function typeOutput() {
-//         g.output.value += emojisArray[counter];
-//         console.log(emojisArray[counter])
-//         counter++;
-//         if (counter >= emojisArray.length) {
-//             clearInterval(interval);
-//         }
-//     }
-
-// }
 function showOutput(output) {
     // makes sure output will not repeat
     g.output.value = '';
@@ -58,51 +30,75 @@ function showOutput(output) {
  * @param {Number} key 
  */
 function encryptMessage() {
+    // key cannot be empty
     if (!g.key.value) {
         return;
     }
+
+    // variables
+    let output = '', inputChar, outputChar;
     const message = g.input.value;
     let key = g.key.value;
     var constantKey;
-    if (!key.match(/[a-zA-Z]/)) {
+
+    // Parse key into number
+    if (key.match(/[a-zA-Z]/)) {
+        key = asciiKeys.indexOf[key];
+    } else {
         key = emojis.indexOf(key) + 1;
     }
+
+    // for resetting key
     constantKey = key;
-    let output = '', character, code, emoji;
 
     if (modernBrowser) {
         for (let i = 0; i < message.length; i++) {
-            character = message[i];
-            if (character.match(/[a-zA-Z. 0-9!?,-:";()\']/)) {
-                var indexChar = emojis.indexOf(lettersToEmojiObj[character]);
+            inputChar = message[i];
 
-                // make sure key will not be bigger than the array length
+            if (inputChar.match(/[a-zA-Z. 0-9!?,-:";()&%\']/)) {
+                // find index of the input character in emoji form
+                var indexChar = emojis.indexOf(lettersToEmojiObj[inputChar]);
+
+                // make sure key will not be bigger than the array length,
+                // this key is the new index of the shifted emoji
                 if (indexChar + key >= emojis.length) {
                     key = indexChar + key - emojis.length;
                 } else {
                     key += indexChar;
                 }
-                emoji = emojis[key];
-                output += emoji;
+
+                outputChar = emojis[key];
+
+                // append shifted emoji to output
+                output += outputChar;
+
+                // reset key
                 key = constantKey;
             }
         }
     } else {
         for (let i = 0; i < message.length; i++) {
-            character = message[i];
+            inputChar = message[i];
 
             // get code if valid
-            if (character.match(/[a-zA-Z]/) || character.match(/[. 0-9!?,-]/)) {
-                code = message.charCodeAt(i);
-                // console.log(code);
-                if (code >= 65 && code <= 90) {
-                    character = String.fromCharCode(((code - 65 + key) % 26) + 65);
-                } else if (code >= 97 && code <= 122) {
-                    character = String.fromCharCode(((code - 97 + key) % 26) + 97);
+            if (inputChar.match(/[a-zA-Z. 0-9!?,-:";()&%\']/)) {
+                var indexChar = g.asciiKeys.indexOf(inputChar);
+
+                // make sure key will not be bigger than the
+                // array's length when making the new index
+                if (indexChar + key >= g.asciiKeys.length) {
+                    key = indexChar + key - g.asciiKeys.length;
                 } else {
-                    character = String.fromCharCode(code + key);
+                    key += indexChar;
                 }
-                output += character;
+
+                outputChar = g.asciiKeys[key];
+
+                // append shifted emoji to output
+                output += outputChar;
+
+                // reset key
+                key = constantKey;
             }
         }
     }
@@ -115,20 +111,41 @@ function encryptMessage() {
  * @param {String} message 
  * @param {Number} key 
  */
-function decryptMessage(message, key) {
-    let output = '', code;
-    for (let i = 0; i < message.length; i++) {
-        code = message.charCodeAt(i);
-        // console.log(code);
-        if (code >= 65 && code <= 90) {
-            output += String.fromCharCode((code - 65 - key + 26) % 26 + 97);
-        } else if (code >= 97 && code <=122) {
-            output += String.fromCharCode((code - 97 - key + 26) % 26 + 97);
-        } else {
-             output += String.fromCharCode(code - key);
-        }
+function decryptMessage() {
+    g.output.value = '';
+
+    // variables
+    let output = '', inputChar, outputChar;
+    const message = g.input.value;
+    let key = g.key.value;
+    var constantKey;
+
+    // Parse key into number
+    if (key.match(/[a-zA-Z]/)) {
+        key = asciiKeys.indexOf[key];
+    } else {
+        key = emojis.indexOf(key) + 1;
     }
-    console.log(output);
+
+    // for resetting key
+    constantKey = key;
+
+    if (modernBrowser) {
+        for (let i = 0; i < message.length; i++) {
+            inputChar = message[i];
+            if (inputChar.match(/[a-zA-Z. 0-9!?,-:";()&%\']/)) {
+                // to do
+            }
+        }
+
+    } else {
+
+    }
+
+
+
+
+
 }
 
 /**
@@ -140,14 +157,6 @@ function runWizard() {
         if (window.location.href.indexOf('wizard') === -1) {
             window.location.href = "wizard.html";
         }
-    // const ref = localStorage.getItem('visit');
-
-    // // if this is the first visit, run the wizard
-    // // and set visit value to true;
-    // if (!ref) {
-    //     console.log('This is the wizard');
-    //     localStorage.setItem('visit', true);
-    // }
 }
 
 /**
@@ -192,9 +201,6 @@ function addGridListeners() {
     for (let i = 0; i < g.gridNodes.length; i++) {
         U.addEvent(g.gridNodes[i], 'click', chooseKey);
     }
-    // Array.from(g.gridNodes).forEach(element => {
-    //     U.addEvent(element, 'click', chooseKey);
-    // });
 }
 /**
  * Populates grid in a 5x5 manner using either
@@ -257,12 +263,6 @@ function rightClick() {
             g.gridNodes[i].innerHTML = emojis[g.counter];
             g.counter++;
         }        
-        // Array.from(g.gridNodes).forEach(element => {
-        //     Array.from(element.childNodes).forEach(emoji => {
-        //         emoji.innerHTML = emojis[g.counter];
-        //         g.counter++;
-        //     })
-        // })
     }
 }
 
@@ -293,11 +293,17 @@ function switchClick() {
         U.addEvent(g.sendButton, 'click', decryptMessage);
         g.sendButton.innerHTML = 'decrypt';
         g.encrypt = false;
+
+        g.input.value = 'Type the message to decipher here!';
+
     } else {
         U.removeEvent(g.sendButton, 'click', decryptMessage);
         U.addEvent(g.sendButton, 'click', encryptMessage);
         g.sendButton.innerHTML = 'encrypt';
         g.encrypt = true;
+
+        g.input.disabled = false;
+        g.input.value = 'Type your message to be encrypted here!';
     }
 }
 
